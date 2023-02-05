406ea8aae0028d9e771bf36ee8ed7f2f856bde19
Ammar Shaar <ammarshaar17@gmail.com>
2023-02-05 12:25:43 +0200

    disable inspect

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -55,6 +55,29 @@ const Header = () => {
     navigate("/cart");
   };
 
+  //disable inspect
+  document.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+  });
+
+  document.onkeydown = function (e) {
+    if (e.keyCode === 123) {
+      return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode === "I".charCodeAt(0)) {
+      return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode === "C".charCodeAt(0)) {
+      return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode === "J".charCodeAt(0)) {
+      return false;
+    }
+    if (e.ctrlKey && e.keyCode === "U".charCodeAt(0)) {
+      return false;
+    }
+  };
+
   return (
     <header ref={headerRef}>
       <Container>
